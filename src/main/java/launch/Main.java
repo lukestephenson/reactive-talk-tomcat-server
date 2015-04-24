1f5ed519155716a90bbbebf06cef57880011318d
Luke Stephenson <luke.stephenson@rea-group.com>
2015-04-24 22:07:47 +1000

    Tidy up for talk

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -15,27 +15,24 @@ public class Main {
         String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
 
-//        Executor executor = tomcat.getConnector().getProtocolHandler().getExecutor();
-//        Connector connector = new Connector();
-//        connector.getProtocolHandler().
-//        tomcat.setConnector(connector);
-
         //The port that we should run on can be set into an environment variable
         //Look for that variable and default if it isn't there.
         String webPort = System.getenv("PORT");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = "9090";
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "9000";
         }
-System.out.println("Will run on port " + webPort);
+
+        System.out.println("Will run on port " + webPort);
         tomcat.setPort(Integer.valueOf(webPort));
 
+        // Reduced the number of threads from the default of 200 to 20 to highlight the impact of
+        // blocking threads on responsiveness
         tomcat.getConnector().setAttribute("maxThreads", "20");
         tomcat.getConnector().setAttribute("maxConnections", "250");
 
         StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
-//declare an alternate location for your "WEB-INF/classes" dir:
         File additionWebInfClasses = new File("target/classes");
         VirtualDirContext resources = new VirtualDirContext();
         resources.setExtraResourcePaths("/WEB-INF/classes=" + additionWebInfClasses);
